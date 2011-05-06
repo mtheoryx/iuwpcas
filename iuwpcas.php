8c7bd99e0623e98e6cf3421d4a28c87084019754
David Poindexter <davpoind@iupui.edu>
2011-05-06 15:48:14 -0400

    Altered some names to comply with WP Plugin coding standards. Added PHPDoc comments. Formatted code for better readability.

--- iuwpcas.php
+++ iuwpcas.php
@@ -1,14 +1,78 @@
 <?php
+/**
+ * IU WpCAS
+ *
+ * A free plugin to integrate WordPress with Indiana University's CAS Authentication.
+ * 
+ * This is a plugin to integrate with Indiana University's Central Authentication System (CAS).
+ * The purpose is to offload the handling of users/passwords to a trusted system for authentication
+ * (are they who they say they are?) purposes, while still maintaining control for authorization 
+ * (what is this person allowed to see/do?) from within the backend of the WordPress admin panel. 
+ * Users are added by IU network ID, with no need to worry about choosing passwords. Additionally, 
+ * the maintainer of the blog now doesn't have to handle forgotten passwords or password resets. 
+ * The plugin is provided for free (libre & gratis) to the IU Community (and anyone else who uses a similar CAS system) by the Indiana University UITS Enterprise Web Tech Services team.
+ *
+ * PHP version 5
+ * 
+ * License: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *
+ * @package     IUWpCAS
+ * @author      David R. Poindexter III <davpoind@iupui.edu>
+ * @copyright   2011 Indiana University
+ * @link        https://github.com/mtheoryx/iuwpcas
+ * @license     GPLv2 or later
+ * @version     0.1.0
+ * @since       File available since release 0.1.0
+ */
+
 /*
-Plugin Name: IU WpCAS
-Version: 1.0
-Description: Authenticate WordPress through Indiana University Central Authentication Service
-Author: David R Poindexter III
+    Plugin Name: IU WpCAS
+    Plugin URI: https://github.com/mtheoryx/iuwpcas
+    Description: This is a plugin to integrate with Indiana University's Central Authentication System (CAS).
+    Author: David R Poindexter III
+    Version: 0.1.0
+    Author URI: http://davidrpoindexter.com/
+    License: GPLv2 or later
 */
 
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Used internally by WordPress
+ * 
+ * @var $cas_configured bool
+ */
 $cas_configured = true;
 
-/* plugin hooks into authentication system */
+/**
+ * Hooks into WordPress authentication system
+ */
 add_action('wp_authenticate', array('IUCASAuthentication', 'authenticate'), 10, 2);
 add_action('wp_logout', array('IUCASAuthentication', 'logout'));
 add_action('lost_password', array('IUCASAuthentication', 'disable_function'));
@@ -18,87 +82,159 @@ add_filter('show_password_fields', array('IUCASAuthentication', 'show_password_f
 add_action('check_passwords', array('IUCASAuthentication', 'check_passwords'), 10, 3);
 add_filter('login_url', array('IUCASAuthentication', 'bypass_reauth'));
 
-if (!class_exists('IUCASAuthentication')) {
-  class IUCASAuthentication {    
-
-	function authenticate(&$username, &$password) {
-		global $using_cookie, $cas_configured;
-		$casLogin = "https://cas.iu.edu/cas/login?cassvc=IU&casurl=".get_option('siteurl')."/wp-login.php";
+/**
+ * Checks if the plugin class has already been defined. If not, it defines it here.
+ * 
+ * This is to avoid class name conflicts within WordPress and plugins.
+ */
+if ( !class_exists('IUCASAuthentication') ) {
+	
+	/**
+	 * Plugin class for custom authentication method.
+	 * 
+	 * {{@internal Missing Long Description}}}
+	 * 
+	 * @package IUWpCas
+	 * @since 0.1.0
+	 */
+	class IUCASAuthentication {    
 		
-		/*
-			Are they an IU user?
-		*/
-		if ( !isset($_GET['casticket']) || (empty($_GET['casticket'])) ) { //no cas ticket set
-			wp_redirect($casLogin);
+		/**
+		 * Main authentication method we are hijacking.
+		 * 
+		 * We use a WP hook to 
+		 * 
+		 * @since 0.1.0
+		 * @global bool $using_cookie
+		 * @global bool $cas_configured
+		 *
+		 * @todo Pull IU-specific variables out, and into db or config file
+		 */
+		function authenticate( &$username, &$password ) {
+			global $using_cookie, $cas_configured;
+			
+			/**
+			 * Login URL we are using for CAS authentication for users to get a CAS ticket.
+			 */
+			$cas_login = "https://cas.iu.edu/cas/login?cassvc=IU&casurl=".get_option('siteurl')."/wp-login.php";
+		    
+		    
+			/**
+			 * Check for CAS ticket set in URL parameters.
+			 * 
+			 * If they don't have one set, we need to kick them out to CAS to get a ticket
+			 * set before proceeding.
+			 */
+			if ( !isset($_GET['casticket']) || (empty($_GET['casticket'])) ) {
+				wp_redirect( $cas_login );
+				exit();
+			}
+			
+			/**
+			 * CAS ticket returned as a URL GET parameter.
+			 */
+			$cas_ticket = $_GET['casticket']; //we know they have a cas ticket set
+			
+			/**
+			 * URL we send users to for validation of their CAS ticket
+			 */
+			$cas_validate_url = 'https://cas.iu.edu/cas/validate?cassvc=IU&casticket='.$cas_ticket.'&casurl='.get_option('siteurl').'/wp-login.php';
+		    
+		    /**
+		     * Response back from CAS after ticket validation.
+		     * 
+		     * Possible values:
+		     * -Line 1: yes or no
+		     * -Line 2: IU Network ID or blank
+		     */
+			$lines = file( $cas_validate_url );
+			$cas_response = rtrim( $lines[0] );
+		    
+		    /**
+		     * If ticket was valid, sets the IU Network ID sent back in CAS validation response.
+		     */
+			if ( $cas_response != "no" ) {
+                $cas_user_id = rtrim( $lines[1] );
+            } else {
+                wp_redirect( $cas_login );
+                exit();
+            }
+		
+			/*
+				We know they're in the IU Network.
+				Do they have an account in this wordpress blog?
+			*/
+			$wp_user = get_userdatabylogin( $cas_user_id );
+		
+			if ( $cas_response == 'yes' && !$wp_user ) { 
+				wp_redirect( site_url() );
+				exit();
+			} else {
+				$wp_username = $wp_user->user_login;
+				wp_set_auth_cookie( $wp_user->ID );
+				wp_redirect( site_url('/wp-admin/') );
+				die();
+			}
+		
+		}
+	    
+	    /**
+	     * Sets random user passwords upon new account creation.
+	     * 
+	     * 
+	     * patched Mar 25 2010 by Jonathan Rogers, jonathan, via findyourfans.com
+	     * 
+	     * @param $user string
+	     * @param $pass1 string
+	     * @param $pass2 string
+	     */
+		function check_passwords( $user, $pass1, $pass2 ) {
+			$random_password = substr( md5( uniqid( microtime( ))), 0, 8 );
+			$pass1=$pass2=$random_password;
+		}
+		
+		/**
+		 * Bypasses WP login to IU CAS service to avoid reauthentication via Wordpress
+		 * 
+		 * @param $login_url string
+		 * @return $login_url string
+		 */
+	    function bypass_reauth( $login_url ) {
+			$login_url = 'https://cas.iu.edu/cas/login?cassvc=IU&casurl='.get_option('siteurl').'/wp-login.php';
+	        return $login_url;
+	    }
+	    
+	    /**
+	     * Custom logout funtion to bypass WordPress default logout and provide a custom redirect target.
+	     * 
+	     * @todo Provide users a notification of logout, and option to log out of CAS.
+	     */
+		function logout(){
+			wp_clear_auth_cookie();
+			wp_redirect( get_option('siteurl') );
 			exit();
 		}
 		
-		$casTicket = $_GET['casticket']; //we know they have a cas ticket set
-		
-		//send them out for validation from CAS
-		$casValidateUrl = 'https://cas.iu.edu/cas/validate?cassvc=IU&casticket='.$casTicket.'&casurl='.get_option('siteurl').'/wp-login.php';
-		
-		$lines = file($casValidateUrl);								//response from cas
-		$casResponse = rtrim($lines[0]);							//first line: yes or no response from CAS
-		
-		if ($casResponse != "no") { $casuID = rtrim($lines[1]);	} 	//grab the IU Network ID sent back by CAS
-		else { wp_redirect($casLogin);	exit();	}					//if the ticket is not valid, send them to get one
-		
-		/*
-			We know they're in the IU Network.
-			Do they have an account in this wordpress blog?
-		*/
-		$wp_user = get_userdatabylogin($casuID);
-		
-		if ( $casResponse == 'yes' && !$wp_user ) { 
-			wp_redirect(site_url());
-			exit();
-		} else {
-			$wp_username = $wp_user->user_login;
-			wp_set_auth_cookie($wp_user->ID);
-			wp_redirect( site_url( '/wp-admin/' ));
-			die();
-		}
-		
-		
+		/**
+		 * Disables display of password fields in the user profile page.
+		 *
+		 * We don't use WP authentication, therefore don't need to worry about any WP-specific passwords.
+		 * 
+		 * @param $show_password_fields bool
+		 * @return bool
+		 */
+	    function show_password_fields( $show_password_fields ) {
+	      return false;
+	    }
+	    
+	    /**
+	     * Utility function to disable WP behaviors.
+	     */
+	    function disable_function() {
+	      die('Disabled');
+	    }
+	    
 	}
 	
-	// set the passwords on user creation
-	// patched Mar 25 2010 by Jonathan Rogers jonathan via findyourfans.com
-	function check_passwords( $user, $pass1, $pass2 ) {
-		$random_password = substr( md5( uniqid( microtime( ))), 0, 8 );
-		$pass1=$pass2=$random_password;
-	}
-	
-	/* WP login bypass to IU CAS service */
-    function bypass_reauth($login_url) {
-        $login_url = 'https://cas.iu.edu/cas/login?cassvc=IU&casurl='.get_option('siteurl').'/wp-login.php';
-        return $login_url;
-    }
-	
-	function logout(){
-		wp_clear_auth_cookie();
-		wp_redirect('https://cas.iu.edu');
-		exit();
-	}
-		
-    
-	/* Don't show password fields on user profile page. */
-    function show_password_fields($show_password_fields) {
-      return false;
-    }
-    
-    
-    function disable_function() {
-      die('Disabled');
-    }
-    
-  }
- }
-
-
-
-
-
-
-
+}
+?>
