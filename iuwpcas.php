e2cd128dd2cf353ed3a1d9678a897eaa6836024f
Patrick Proctor <patrick42h@gmail.com>
2012-08-23 17:06:40 -0400

    Changed version numbers back

--- iuwpcas.php
+++ iuwpcas.php
@@ -32,18 +32,18 @@
  * @package     IUWpCAS
  * @author      David R. Poindexter III <davpoind@iupui.edu>
  * @copyright   2011 Indiana University
- * @link        https://github.com/patproct/iuwpcas
+ * @link        https://github.com/mtheoryx/iuwpcas
  * @license     GPLv2 or later
- * @version     0.2.1
+ * @version     0.2.0
  * @since       File available since release 0.1.0
  */
 
 /*
     Plugin Name: IU WP CAS
-    Plugin URI: https://github.com/patproct/iuwpcas
+    Plugin URI: https://github.com/mtheoryx/iuwpcas
     Description: This is a plugin to integrate with Indiana University's Central Authentication System (CAS).
     Author: David R Poindexter III
-    Version: 0.2.1
+    Version: 0.1.0
     Author URI: http://davidrpoindexter.com/
     License: GPLv2 or later
 */
